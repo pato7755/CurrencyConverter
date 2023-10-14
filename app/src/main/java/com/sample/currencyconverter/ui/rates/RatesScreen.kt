cf7aba8f94f66e6608fae9ec0a0dd86e613cbc3e
Patrick Essiam <pat.essiam701@gmail.com>
2023-10-14 21:24:56 +0000

    Add spacing between flags and text on Rates screen

--- app/src/main/java/com/sample/currencyconverter/ui/rates/RatesScreen.kt
+++ app/src/main/java/com/sample/currencyconverter/ui/rates/RatesScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
@@ -131,6 +132,7 @@ fun RatesItem(
                 .align(Alignment.CenterVertically)
                 .absolutePadding(top = 15.dp)
         )
+        Spacer(modifier = Modifier.size(20.dp))
         Column(
             modifier = Modifier
                 .padding(16.dp),
